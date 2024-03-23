74c7f7ce6bd9b86feda14ec9369a5e6c42a64f36
Nico Krause <post@nicokrause.com>
2024-03-23 12:39:35 +0500

    adding proxy_write_timeout 12h;

--- relay/src/relay.js
+++ relay/src/relay.js
@@ -1,25 +1,25 @@
-import { yamux } from '@chainsafe/libp2p-yamux'
 import { createLibp2p } from 'libp2p'
+import { identify } from '@libp2p/identify'
+import { yamux } from '@chainsafe/libp2p-yamux'
 import { noise } from '@chainsafe/libp2p-noise'
 import { circuitRelayTransport,circuitRelayServer } from '@libp2p/circuit-relay-v2'
-import { tcp } from '@libp2p/tcp'
-import { webSockets } from '@libp2p/websockets'
-import * as filters from '@libp2p/websockets/filters'
-import { identify } from '@libp2p/identify'
 import { createFromPrivKey } from '@libp2p/peer-id-factory'
 import { unmarshalPrivateKey } from '@libp2p/crypto/keys'
 import { fromString as uint8ArrayFromString } from 'uint8arrays/from-string'
-import {bootstrap} from "@libp2p/bootstrap";
-import {pubsubPeerDiscovery} from "@libp2p/pubsub-peer-discovery";
-import {ping} from "@libp2p/ping";
-import {autoNAT} from "@libp2p/autonat";
-import {dcutr} from "@libp2p/dcutr";
-import {gossipsub} from "@chainsafe/libp2p-gossipsub";
+import { bootstrap } from "@libp2p/bootstrap";
+import { pubsubPeerDiscovery } from "@libp2p/pubsub-peer-discovery";
+import { ping } from "@libp2p/ping";
+import { autoNAT } from "@libp2p/autonat";
+import { dcutr } from "@libp2p/dcutr";
+import { gossipsub } from "@chainsafe/libp2p-gossipsub";
+import { tcp } from '@libp2p/tcp'
+import { webSockets } from '@libp2p/websockets'
+import * as filters from '@libp2p/websockets/filters'
 import 'dotenv/config'
 
 // import {bootstrapConfig} from "./config.js";
 
-// output of: console.log(server.peerId.privateKey.toString('hex'))
+export const CONTENT_TOPIC = "/dContact/3/message/proto";
 const relayPrivKey = process.env.RELAY_PRIVATE_KEY;
 // the peer id of the above key
 // const relayId = '12D3KooWAJjbRkp8FPF5MKgMU53aUTxWkqvDrs4zc1VMbwRwfsbE'
@@ -33,16 +33,19 @@ const server =
 		peerId,
 		addresses: {
 			listen: [
-				'/ip4/0.0.0.0/tcp/1234',
+				'/ip4/0.0.0.0/tcp/1235',
+				'/ip4/0.0.0.0/udp/9092/quic',
 				'/ip4/0.0.0.0/tcp/12345/ws'],
 			announce: [
-				// '/ip4/0.0.0.0/tcp/1234',
+				'/ip4/0.0.0.0/tcp/1235',
+				'/ip4/0.0.0.0/udp/9092/quic',
+				'/ip4/0.0.0.0/tcp/12345/ws',
 				'/dns4/ipfs.le-space.de/tcp/1234',
 				'/dns4/ipfs.le-space.de/tcp/443/wss'
 			]
 		},
 		transports: [
-			circuitRelayTransport(),
+			circuitRelayTransport({discoverRelays:2}),
 			tcp(),
 			webSockets({
 				filter: filters.all
@@ -52,7 +55,10 @@ const server =
 		streamMuxers: [yamux()],
 		peerDiscovery: [
 			bootstrap({
-				list: ['/ip4/159.69.119.82/udp/9091/quic-v1/p2p/12D3KooWF5fGyE4VeXMhSGd9rCwckyxCVkA6xfoyFJG9DWJis62v']}),
+				list: [
+					'/ip4/127.0.0.1/udp/9091/quic-v1/p2p/12D3KooWKABDpSmjtXQRm1FmkwbRDMzLHYi83btQnxHMAYRpYH8k' +
+				'/ip4/127.0.0.1/udp/9091/quic/p2p/12D3KooWKABDpSmjtXQRm1FmkwbRDMzLHYi83btQnxHMAYRpYH8k']
+			}),
 			pubsubPeerDiscovery({
 				interval: 1000,
 				topics: ['dev-dcontact._peer-discovery._p2p._pubsub','dcontact._peer-discovery._p2p._pubsub'], // defaults to ['_peer-discovery._p2p._pubsub']
@@ -84,6 +90,7 @@ server.addEventListener('peer:disconnect', async event => {
 	console.log('peer:disconnect', event.detail)
 	server.peerStore.delete(event.detail)
 })
+server.services.pubsub.subscribe(CONTENT_TOPIC)
 
 console.log(server.peerId.toString())
 console.log('p2p addr: ', server.getMultiaddrs().map((ma) => ma.toString()))
